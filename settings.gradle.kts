2a9aaa2a749da2b72bafeabe69f4e3d230e0b7b9
Hoang Nguyen <90430178+hoangnguyen2021@users.noreply.github.com>
2022-12-17 14:30:19 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "InstaClone-Ktor"
---
+++ src/main/kotlin/hoang/myapp/Application.kt
@@ -0,0 +1,16 @@
+package hoang.myapp
+
+import io.ktor.server.application.*
+import hoang.myapp.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureSockets()
+    configureRouting()
+}
