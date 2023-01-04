02c56d520bf2f452d75f2de186d09d027899350e
Hoang Nguyen <90430178+hoangnguyen2021@users.noreply.github.com>
2023-01-04 03:07:20 -0500

    Fix logic error

--- src/main/kotlin/hoang/myapp/security/verification/TwilioVerificationService.kt
+++ src/main/kotlin/hoang/myapp/security/verification/TwilioVerificationService.kt
@@ -3,7 +3,6 @@ package hoang.myapp.security.verification
 import com.twilio.rest.verify.v2.service.Verification
 import com.twilio.rest.verify.v2.service.VerificationCheck
 import hoang.myapp.config.Config.TWILIO_VERIFY_SERVICE_SID
-import hoang.myapp.security.verification.VerificationService
 import kotlinx.coroutines.future.await
 
 class TwilioVerificationService : VerificationService {
@@ -24,14 +23,14 @@ class TwilioVerificationService : VerificationService {
 
     override suspend fun checkVerificationToken(recipient: String, tokenToCheck: String): Boolean {
         return try {
-            VerificationCheck.creator(
+            val verificationCheck = VerificationCheck.creator(
                 TWILIO_VERIFY_SERVICE_SID
             )
                 .setTo(recipient)
                 .setCode(tokenToCheck)
                 .createAsync()
                 .await()
-            true
+            verificationCheck.status == "approved"
         } catch (e: Exception) {
             false
         }
